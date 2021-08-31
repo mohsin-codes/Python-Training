eb7b89c6c20b120b7edb52067e737988cb4e382d
Mohsin <mohsink9899@gmail.com>
2021-08-31 19:08:48 +0530

    Variables basics and small exercise

---
+++ Variables/variables_basics.py
@@ -0,0 +1,17 @@
+# integer Values
+price = 40
+print(price)
+
+#Floating point values
+rating = 9.5
+print(rating)
+
+#Boolean Values
+is_true = True
+print(is_true)
+is_true = False
+print(is_true)
+
+#string values 
+name = "Mohsin Khan"
+print(name)
---
+++ Variables/variables_exercise.py
@@ -0,0 +1,6 @@
+patient_name = "John Smith"
+patient_age = 20
+new_patient = True
+
+
+print (patient_name, patient_age, new_patient)
