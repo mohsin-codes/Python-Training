c2aa5f37f05c892f0b47d188d472c8bae44551e0
Mohsin <mohsink9899@gmail.com>
2021-08-31 20:08:12 +0530

    String and String Slicing

---
+++ Strings/string_slicing.py
@@ -0,0 +1,15 @@
+from typing import Counter
+
+
+course = "Python for Beginners"
+#Different types of Slicing
+
+print(course[0:])
+print(course[:])
+print(course[:5])
+print(course[2:8])
+print(course[:-3])
+print(course[1:-1])
+
+another = course[:]
+print(another)
---
+++ Strings/strings_basic.py
@@ -0,0 +1,19 @@
+#String containing Apostrophe
+course = "Python's for Beginner"
+print(course)
+
+#String containing Double inverted Comma's
+course2 = 'Python for "Beginners"'
+print(course2)
+
+
+#Multi Line String
+email = '''
+Hi Tim,
+
+Hope you're doing fine. Let's catch up some day at "Puzzles"
+
+Regards,
+Red Skull
+'''
+print(email)
