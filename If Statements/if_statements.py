e83099a0ad329619030dbc1c6a628de21fc0cd8b
Mohsin <mohsink9899@gmail.com>
2021-09-01 19:20:26 +0530

    If Statements

---
+++ If Statements/if_statements.py
@@ -0,0 +1,7 @@
+day = input("How is the day? ")
+if (day == "hot"):
+    print("It's a hot day.\nDrink Plenty of water!")
+elif (day == "cold"):
+    print("It's a cold day.\nWear warm clothes.")
+else:
+    print("It is a beautiful day!")
