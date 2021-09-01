885a48b2c565bb1f54cee885bfdc733d2c970ed9
Mohsin <mohsink9899@gmail.com>
2021-09-01 18:34:24 +0530

    Arithmetic Operator

---
+++ Arithmetic Operators/arithmetic_operators.py
@@ -0,0 +1,22 @@
+# Addition Operator
+print(10 + 3)
+
+# Subtraction Operator
+print(10 - 3)
+
+# Multiplication Operator
+print(10 * 3)
+
+# Division Operator
+print(10 / 3)
+
+# Integer Division Operator
+print(10 // 3)
+
+# Modulus Operator
+print(10 % 3) 
+
+# augmented assignment Operator
+x = 20
+x += 10
+print(x)
