68ce2ac19630268168ebb793b4524dd2e2e0863f
Mohsin <mohsink9899@gmail.com>
2021-09-01 18:16:22 +0530

    String Operators

---
+++ String Operators/string_operators.py
@@ -0,0 +1,24 @@
+course = "Python for Beginners"
+
+# Len Function
+print(len(course))
+
+# Upper Method
+print(course.upper())
+
+# Lower Method
+print(course.lower())
+
+# Title Method
+print(course.title())
+
+# Find Method
+print(course.find("o"))
+print(course.find("B"))
+print(course.find("e"))
+
+# Replace Method
+print(course.replace("Beginners", "Absolute Beginners"))
+
+# in keyword
+print("Python" in course)
