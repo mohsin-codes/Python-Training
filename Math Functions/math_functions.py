cc79c1d616e60582028739c865faf6a51c08de93
Mohsin <mohsink9899@gmail.com>
2021-09-01 19:03:48 +0530

    Math Functions

---
+++ Math Functions/math_functions.py
@@ -0,0 +1,12 @@
+import math
+
+
+print(math.ceil(2.9))
+print(math.floor(2.9))
+print(math.factorial(5))
+print(math.fabs(-23))
+print(round(2.9))
+print(round(2.1))
+print(round(2.5))
+print(abs(2.9))
+print(abs(-2.9))
