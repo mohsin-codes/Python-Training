010dee55a57e627668ee698162374d908e6c1237
Mohsin <mohsink9899@gmail.com>
2021-09-02 15:05:08 +0530

    For Loop and small exercise

---
+++ For Loop/for_loop.py
@@ -0,0 +1,19 @@
+# iterating over a string
+for item in "Python":
+    print(item)
+
+#iterating over a list
+for item in ["Mohsin", "Ashar", "Zayn", "Samra"]:
+    print(item)
+
+# iterating over a range 
+for item in range(10):
+    print(item)
+
+#iterating over a range with an initial value
+for item in range(5,10):
+    print(item)
+
+#iterating over a range with an initial value with a step counter
+for item in range(5,10,2):
+    print(item)
