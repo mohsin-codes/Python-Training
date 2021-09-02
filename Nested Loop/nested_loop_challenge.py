4d5fcb7d2a720e80180a651f05768ceb75f49038
Mohsin <mohsink9899@gmail.com>
2021-09-02 15:29:14 +0530

    Nested Loop and small exercise

---
+++ Nested Loop/nested_loop_challenge.py
@@ -0,0 +1,16 @@
+numbers = [5,2,5,2,2]
+for i in range(1):
+
+    for j in numbers:
+        print()
+        print("X"*j, end="")
+    print()
+
+
+numbers = [2,2,2,2,5]
+for i in range(1):
+
+    for j in numbers:
+        print()
+        print("X"*j, end="")
+    print()
