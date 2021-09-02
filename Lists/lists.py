760460eeaa9ea890d49be8fbec36ff4e801f0d4e
Mohsin <mohsink9899@gmail.com>
2021-09-02 16:12:10 +0530

    Lists

---
+++ Lists/lists.py
@@ -0,0 +1,18 @@
+person = ['john', 'bob', 'parker', 'Lucy', 'Jake']
+#Printing the list
+print(person)
+
+#Printing list with index
+print(person[1])
+print(person[4])
+print(person[3])
+
+#Printing list with negative index
+print(person[-1])
+print(person[-2])
+
+#Slicing through the list
+print(person[2:])
+print(person[2:4])
+print(person[:3])
+print(person[:])
