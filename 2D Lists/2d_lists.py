43bab2751b6a827940a5bd6d5d701ad2803c9d7b
Mohsin <mohsink9899@gmail.com>
2021-09-02 16:33:04 +0530

    2D List

---
+++ 2D Lists/2d_lists.py
@@ -0,0 +1,13 @@
+matrix =[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]
+
+print(matrix[0][2])
+print(matrix[2][2])
+
+for row in range(1):
+    for element in matrix:
+        print(element)
+    print()
