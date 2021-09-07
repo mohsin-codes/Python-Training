96493b9cfab4fd3443d21163099535c959ae329c
Mohsin <mohsink9899@gmail.com>
2021-09-07 17:06:40 +0530

    Added new model - offer

--- Building a website with Django/PyShop/products/models.py
+++ Building a website with Django/PyShop/products/models.py
@@ -5,3 +5,8 @@ class Product(models.Model):
     price = models.FloatField()
     stock = models.IntegerField()
     image = models.CharField(max_length=2083)
+
+class Offer(models.Model):
+    code = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
